9b09780fa508e932c78ea644c110ceb840cbcd57
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-03 04:02:41 +0100

    Cross Origin

--- src/main/java/fr/sauveteurdudunkerquois/backend/app/BackendController.java
+++ src/main/java/fr/sauveteurdudunkerquois/backend/app/BackendController.java
@@ -2,14 +2,12 @@ package fr.sauveteurdudunkerquois.backend.app;
 
 import fr.sauveteurdudunkerquois.backend.data.DataIdentity;
 import fr.sauveteurdudunkerquois.backend.data.article.Article;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.StreamSupport;
 
+@CrossOrigin
 @RestController
 public class BackendController {
     private final BackendRepository repo;
